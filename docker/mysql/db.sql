6349bc8cc7c62866ecac5f4fabdfd2887121fccf
Nguyen Le <nguyen.le@ptnglobalcorp.com>
2022-06-28 11:38:50 +0700

    Add opencart service and update docker

--- docker/mysql/db.sql
+++ docker/mysql/db.sql
@@ -1,9 +1,15 @@
-# create databases
+-- opencart
+CREATE DATABASE IF NOT EXISTS `opencart`;
+
+-- lumen api gateway
 CREATE DATABASE IF NOT EXISTS `lumen_api`;
 CREATE DATABASE IF NOT EXISTS `lumen_product`;
 CREATE DATABASE IF NOT EXISTS `lumen_order`;
 
-# create root user and grant rights
-CREATE USER 'lumen'@'localhost' IDENTIFIED BY 'local';
-GRANT ALL PRIVILEGES ON *.* TO 'lumen'@'%';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
+
+-- create user and grant rights
+CREATE USER 'opencart'@'localhost' IDENTIFIED BY 'opencart';
+CREATE USER 'lumen'@'localhost' IDENTIFIED BY 'lumen';
+GRANT ALL PRIVILEGES ON *.* TO 'opencart'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'lumen'@'%';
---
+++ docker/opencart/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# API Gateway
+
+declare -a arr=("ApiGateway" "OrdersService" "ProductsApi")
+
+for i in "${arr[@]}"
+do
+   cd /var/www/$i
+   # Permission
+    if [ "$(stat -c "%U" storage/logs/)" != "www-data" ] || [ "$(stat -c "%G" storage/logs/)" != "www-data" ]; then
+        rm -rf sstorage/logs/*
+        chown -R www-data:www-data storage/logs/
+    fi
+
+    if [ ! -d "vendor" ]; then
+        composer install
+    fi
+
+    if [ ! -f ".env" ]; then
+        cp .env.example .env
+    fi
+
+    if [ "$(php artisan migrate:status)" = "Migration table not found." ]; then
+        php artisan passport:install
+    fi
+done
+
+# Run
+php-fpm
